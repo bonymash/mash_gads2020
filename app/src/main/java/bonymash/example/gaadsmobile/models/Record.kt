8acf2641f8c93d7ffac8ed10275b33421fdf5845
Mash <bonymash@gmail.com>
2020-09-11 14:25:34 -0700

    Initial commit

---
+++ app/src/main/java/bonymash/example/gaadsmobile/models/Record.kt
@@ -0,0 +1,10 @@
+package bonymash.example.gaadsmobile.models
+
+
+data class Record(
+    var hours: Int,
+    var score: Int,
+    var name: String,
+    var country: String,
+    var badgeUrl: String
+)
---
+++ app/src/main/java/bonymash/example/gaadsmobile/ui/activities/MainActivity.kt
@@ -0,0 +1,33 @@
+package bonymash.example.gaadsmobile.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.gaadsmobile.R
+import bonymash.example.gaadsmobile.adapters.SectionsPagerAdapter
+import com.example.gaadsmobile.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        mBinding.viewPager.adapter = sectionsPagerAdapter
+
+        mBinding.tabs.setupWithViewPager(mBinding.viewPager)
+
+        mBinding.btnSubmit.setOnClickListener {
+            startActivity(Intent(this, SubmitActivity::class.java))
+        }
+
+    }
+
+
+}
